31c47db23262fc6e4e01b656bc5788f51fe3a33f
Yehan20 <yehansk15@gmail.com>
2023-05-11 00:58:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-red-900'>
+       <h1 className=''>Hi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
