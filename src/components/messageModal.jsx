76facdbcbbea84c307adea818430a68178b39016
Yehan20 <yehansk15@gmail.com>
2023-05-17 22:16:55 +0530

    .

--- src/components/messageModal.jsx
+++ src/components/messageModal.jsx
@@ -10,7 +10,7 @@ const MessageModal = ({message,canRedirect,redirectLogin}) => {
         <div className="fixed inset-0 " id="modal-overlay">
             <div className="w-full px-10 text-center modal-form fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg " id="modal-content">
                 <p className="text-3xl text-greenColor mb-4">{message}</p>
-                {!canRedirect && <button onClick={closeModal} className="hover:opacity-75 modal-button text-lg inline-block bg-greenColor text-white px-7 py-2">
+                {(!canRedirect | !redirectLogin ) && <button onClick={closeModal} className="hover:opacity-75 modal-button text-lg inline-block bg-greenColor text-white px-7 py-2">
                     Okay
                 </button>}
                 {canRedirect && <Link  className="modal-button  hover:opacity-75 text-lg inline-block bg-greenColor text-white px-7 py-2" to={'/contacts/all'}> Okay </Link>}
