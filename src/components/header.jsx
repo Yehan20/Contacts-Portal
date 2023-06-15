eadb862a8597a20f42b96d7e7075dcf17771dde9
Yehan20 <yehansk15@gmail.com>
2023-06-15 11:35:21 +0530

    .

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import Logo from '../assets/images/twcLogo.svg'
 const Header = () => {
   return (
     <div className='text-white mb-5 lg:mb-16'>
-      <Link to='/' title='Go Home'><img src={Logo} alt="Logo" /></Link>
+      <Link to='/' title='Go Home'><img src={Logo} className='w-8' alt="Logo" /></Link>
       <h3 className='text-3xl font-bold'>contacts <br />
        <span className='font-normal'>portal</span> 
       </h3>
